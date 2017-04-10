3ee60f8bb2d7fe309a54496eb1a9fcef11096d2b
harshana <jmhmjayamaha@gmail.com>
2017-04-10 23:09:38 +0530

    added model classes

---
+++ hibernate_ManyToMany/src/main/java/lk/harshana/model/Course.java
@@ -0,0 +1,45 @@
+package lk.harshana.model;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Course")
+public class Course {
+
+	@Id
+	private String courseCode;
+	private String courseName;
+
+	@ManyToMany(mappedBy = "courses")
+	private Set<Student> students;
+
+	public Set<Student> getStudents() {
+		return students;
+	}
+
+	public void setStudents(Set<Student> students) {
+		this.students = students;
+	}
+
+	public String getCourseCode() {
+		return courseCode;
+	}
+
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
+}
