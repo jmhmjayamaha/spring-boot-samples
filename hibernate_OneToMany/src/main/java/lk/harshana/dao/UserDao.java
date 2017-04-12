ba67339b5a64ef1af7fd16fa35ed1161d160f1b0
harshana <jmhmjayamaha@gmail.com>
2017-04-12 06:42:11 +0530

    dao classes are added

---
+++ hibernate_OneToMany/src/main/java/lk/harshana/dao/UserDao.java
@@ -0,0 +1,9 @@
+package lk.harshana.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import lk.harshana.model.User;
+
+public interface UserDao extends CrudRepository<User, Long> {
+
+}
---
+++ hibernate_OneToMany/src/main/java/lk/harshana/dao/VehicleDao.java
@@ -0,0 +1,9 @@
+package lk.harshana.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import lk.harshana.model.Vehicle;
+
+public interface VehicleDao extends CrudRepository<Vehicle, Long>{
+
+}
