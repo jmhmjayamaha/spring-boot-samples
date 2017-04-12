a051b9e22b3060be0db4ae3b8fefad0724bf2e62
harshana <jmhmjayamaha@gmail.com>
2017-04-12 06:40:30 +0530

    user , vehicle model class is added

---
+++ hibernate_OneToMany/src/main/java/lk/harshana/model/User.java
@@ -0,0 +1,56 @@
+package lk.harshana.model;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
+
+@Entity
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1l;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String name;
+	
+	@OneToMany(cascade = CascadeType.ALL , fetch = FetchType.EAGER)
+	@JoinTable(name="user_vihicle" , joinColumns = @JoinColumn(name="user_id"),
+	inverseJoinColumns = @JoinColumn(name="vehicle_id"))
+	private Set<Vehicle> vehicle = new HashSet<Vehicle>();
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<Vehicle> getVehicle() {
+		return vehicle;
+	}
+
+	public void setVehicle(Set<Vehicle> vehicle) {
+		this.vehicle = vehicle;
+	}
+	
+}
