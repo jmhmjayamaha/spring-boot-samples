67d0b7b996a97974c52bf4cb4f6578015338ef93
jmhmjayamaha <jmhmjayamaha@gmail.com>
2017-04-27 15:38:11 +0530

    add controler to test

---
+++ spring_boot_test_restdoc/src/main/java/lk/harshana/HelloController.java
@@ -0,0 +1,16 @@
+package lk.harshana;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public Map<String, Object> greeting() {
+		return Collections.singletonMap("message", "Hello World");
+	}
+}
