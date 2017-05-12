e39df36dd30cdb7606540a9dd8ae3504608a99ec
jmhmjayamaha <jmhmjayamaha@gmail.com>
2017-05-12 22:01:40 +0530

    change the main class name

--- spring_boot_actuator/src/test/java/lk/harshana/ApplicationTests.java
+++ spring_boot_actuator/src/test/java/lk/harshana/ApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootActuatorApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
