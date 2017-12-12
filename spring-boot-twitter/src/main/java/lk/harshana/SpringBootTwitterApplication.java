d5f1f36ffb0f1dcda90d8efac25b6d5df2e0cb26
Harshana Madusanka Jayamaha <jmhmjayamaha@gmail.com>
2017-12-12 12:53:56 +0530

    access twitter data

---
+++ spring-boot-twitter/src/main/java/lk/harshana/SpringBootTwitterApplication.java
@@ -0,0 +1,12 @@
+package lk.harshana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTwitterApplication.class, args);
+	}
+}
